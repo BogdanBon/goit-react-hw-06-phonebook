227dded903c0f1e3d4ffb37c2a9f9d94921c3858
Bogdan Bondarenko <77720392+BogdanBon@users.noreply.github.com>
2021-10-11 20:52:19 +0200

    bugfix

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -54,7 +54,7 @@ export default function ContactForm() {
         value={name}
         onChange={handleChange}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        title="The name can only consist of letters, apostrophes, dashes and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
         required
       />
       <label className={s.label} htmlFor="inputPhone">
@@ -69,7 +69,7 @@ export default function ContactForm() {
         value={number}
         onChange={handleChange}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
       <button className={s.btn} type="submit">
